e4755ae03549d0ff095dc71cc726d1723bdef658
dburk3 <daemon_dreams@protonmail.com>
2018-12-21 19:54:20 -0800

    Work'd it out4u <3

--- dango/urls.py
+++ dango/urls.py
@@ -21,4 +21,4 @@ urlpatterns = [
     path('', index),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
